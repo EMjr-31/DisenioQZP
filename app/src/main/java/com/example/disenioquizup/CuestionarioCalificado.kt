21149e699d07630f79620cc57194e89fd37419e5
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-05-11 16:32:45 -0600

    Disenio de las interfaces, pregunta, terminado, califiacion y menu de usuario terminados

--- app/src/main/java/com/example/disenioquizup/CuestionarioCalificado.kt
+++ app/src/main/java/com/example/disenioquizup/CuestionarioCalificado.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MenuUsuario : AppCompatActivity() {
+class CuestionarioCalificado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_usuario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menuusuarios)) { v, insets ->
+        setContentView(R.layout.activity_cuestionario_calificado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
